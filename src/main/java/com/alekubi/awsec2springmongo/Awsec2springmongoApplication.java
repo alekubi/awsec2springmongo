df175eab3d2dd409dfe34a80c13873896e1d83de
Alexander Kubis <alexanderkubis@gmx.de>
2023-05-29 11:12:53 +0200

    Commit spring boot project initialized

---
+++ src/main/java/com/alekubi/awsec2springmongo/Awsec2springmongoApplication.java
@@ -0,0 +1,13 @@
+package com.alekubi.awsec2springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awsec2springmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awsec2springmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alekubi/awsec2springmongo/Awsec2springmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alekubi.awsec2springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awsec2springmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
